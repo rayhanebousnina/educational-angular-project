e4231c78605ef67f00c30399353428be104830ee
rayhanebousnina <bousninarayhanesimplonenda@gmail.com>
2022-02-23 02:01:10 +0100

    add event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'educational-angular-project';
+  title = 'TESTING';
+  showProduct: Boolean = false;
+  products: any[] = [
+    {
+      productId: 1,
+      productName: 'Cardigan femme',
+      productPrice: '85',
+    },
+    {
+      productId: 2,
+      productName: 'Robe noir',
+      productPrice: '65',
+    },
+  ];
+
+  toggleProduct(): void {
+    console.log('tesssst', this.showProduct);
+    this.showProduct = !this.showProduct;
+  }
 }
+
+// export class TabMenuDemo {
+//   items: MenuItem[] | undefined;
+
+//   ngOnInit() {
+//     this.items = [
+//       { label: 'Home', icon: 'pi pi-fw pi-home' },
+//       { label: 'Calendar', icon: 'pi pi-fw pi-calendar' },
+//       { label: 'Edit', icon: 'pi pi-fw pi-pencil' },
+//       { label: 'Documentation', icon: 'pi pi-fw pi-file' },
+//       { label: 'Settings', icon: 'pi pi-fw pi-cog' },
+//     ];
+//   }
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabMenuModule } from 'primeng/tabmenu';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TabMenuModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
