1df89638fa90bf407608d7abbd62d503170a58b3
rayhanebousnina <bousninarayhanesimplonenda@gmail.com>
2022-02-25 14:50:38 +0100

    add contact page

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ContactComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
