b49c513c64cad0ffd68aaecb147c34476ce646a9
rayhanebousnina <bousninarayhanesimplonenda@gmail.com>
2022-02-26 00:06:36 +0100

    adding map to contact page

--- src/app/features/dashboard/contact/contact.component.ts
+++ src/app/features/dashboard/contact/contact.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  options: any;
+  constructor() {}
 
   ngOnInit(): void {
+    this.options = {
+      center: { lat: 36.890257, lng: 30.707417 },
+      zoom: 12,
+    };
   }
-
 }
--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -4,12 +4,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { GMapModule } from 'primeng/gmap';
 
 @NgModule({
   declarations: [HomeComponent, ContactComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    GMapModule,
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts'),
     }),
