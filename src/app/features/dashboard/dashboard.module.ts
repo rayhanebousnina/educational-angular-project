8f6467d29d2308b51cc61028fe468bce7d4adf69
rayhanebousnina <bousninarayhanesimplonenda@gmail.com>
2022-03-10 00:57:38 +0100

    creating child components for dashboard

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -5,9 +5,13 @@ import { NgxEchartsModule } from 'ngx-echarts';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
+import { DonutComponent } from './home/donut/donut.component';
+import { BarComponent } from './home/bar/bar.component';
+import { RadarComponent } from './home/radar/radar.component';
+import { LineComponent } from './home/line/line.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, DonutComponent, BarComponent, RadarComponent, LineComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
